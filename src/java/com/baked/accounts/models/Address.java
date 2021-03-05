7da4e507343784a37cb49c26525c840a72505427
Brendanduplessis <62245868+Brendanduplessis@users.noreply.github.com>
2021-03-05 13:11:53 +0200

    Brendan adding the DAO Accounts with 2 working methods LOG IN Sign In

--- src/java/com/baked/accounts/models/Address.java
+++ src/java/com/baked/accounts/models/Address.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.baked.accounts.models;
 
 /**
  *
- * @author khwezi
+ * @author UnicornBrendan
  */
 public class Address {
     private String id;
--- src/java/com/baked/products/dao/ProductsDAOInterface.java
+++ src/java/com/baked/products/dao/ProductsDAOInterface.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.baked.products.dao;
 
 import com.baked.products.models.Category;
@@ -13,7 +9,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author khwezi
+ * @author UnicornBrendan
  */
 public interface ProductsDAOInterface {
     ArrayList<Recipe> getRecipes(String productName);
